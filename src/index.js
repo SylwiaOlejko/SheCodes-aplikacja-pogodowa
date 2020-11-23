97b0d5ed439014b4ab7e7313f9898ff582e8260a
Sylwia Olejko <sylwia.olejko@gmail.com>
2020-11-23 16:00:24 +0100

    pozycjonowanie

--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
+
+
 let weather = {
     "paris": {
-      temp: 19.7,
-      humidity: 80
+    temp: 19.7,
+    humidity: 80,
     },
+
     "tokyo": {
-      temp: 17.3,
-      humidity: 50
+    temp: 17.3,
+    humidity: 50,
     },
+
     "lisbon": {
-      temp: 30.2,
-      humidity: 20
+    temp: 30.2,
+    humidity: 20,
     },
+
     "san francisco": {
-      temp: 20.9,
-      humidity: 100
+    temp: 20.9,
+    humidity: 100,
     },
+
     "moscow": {
-      temp: -5,
-      humidity: 20
+    temp: -5,
+    humidity: 20,
     }
-  };
-  
+};
